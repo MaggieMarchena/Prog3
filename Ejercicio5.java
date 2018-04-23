afc108065488fc17e57167f26e05186f6980b1e7
MaggieMarchena <maga11_tandil@hotmail.com>
2018-04-23 19:35:32 -0300

    Problemas solucionados
    Agregada funcionalidad faltante, encapsulamiento, errores arreglados

--- Ejercicio5.java
+++ Ejercicio5.java
@@ -7,33 +7,36 @@ public class Ejercicio5 {
 		MySimpleLinkedList list1 = new MySimpleLinkedList();
 		MySimpleLinkedList list2 = new MySimpleLinkedList();
 		
-		Node n1 = new Node(4,null);
-		Node n2 = new Node(3,null);
-		Node n3 = new Node(15,null);
-		Node n4 = new Node(28,null);
-		Node n5 = new Node(80,null);
-		Node n6 = new Node(49,null);
+		MySimpleLinkedList list3 = new MySimpleLinkedList();
+		MySimpleLinkedList list4 = new MySimpleLinkedList();
 		
-		list1.insert(n1);
-		list1.insert(n2);
-		list1.insert(n3);
-		list1.insert(n4);
-		list1.insert(n5);
-		list1.insert(n6);
-
-		Node n7 = new Node(28,null);
-		Node n8 = new Node(4,null);
-		Node n9 = new Node(47,null);
-		Node n10 = new Node(1,null);
-		Node n11 = new Node(80,null);
-		Node n12 = new Node(20,null);
+		list1.insertFirst(4);
+		list1.insertFirst(3);
+		list1.insertFirst(15);
+		list1.insertFirst(28);
+		list1.insertFirst(80);
+		list1.insertFirst(49);
 		
-		list2.insert(n7);
-		list2.insert(n8);
-		list2.insert(n9);
-		list2.insert(n10);
-		list2.insert(n11);
-		list2.insert(n12);
+		list2.insertFirst(28);
+		list2.insertFirst(4);
+		list2.insertFirst(47);
+		list2.insertFirst(3);
+		list2.insertFirst(80);
+		list2.insertFirst(20);
+		
+		list3.insertFirst(80);
+		list3.insertFirst(49);
+		list3.insertFirst(28);
+		list3.insertFirst(15);
+		list3.insertFirst(4);
+		list3.insertFirst(3);
+		
+		list4.insertFirst(80);
+		list4.insertFirst(47);
+		list4.insertFirst(28);
+		list4.insertFirst(20);
+		list4.insertFirst(4);
+		list4.insertFirst(3);
 		
 		System.out.println("Lista 1: ");
 		list1.printList();
@@ -42,20 +45,43 @@ public class Ejercicio5 {
 		System.out.println("Lista combinada: ");
 		MySimpleLinkedList resultList = mergeListsSorted(list1, list2);		
 		resultList.printList();	
+		
+		System.out.println("Lista 3: ");
+		list3.printList();
+		System.out.println("Lista 4: ");
+		list4.printList();	
+		System.out.println("Lista combinada: ");
+		MySimpleLinkedList resultList2 = mergeSortedLists(list3, list4);		
+		resultList2.printList();	
 	}
 	
 	private static MySimpleLinkedList mergeListsSorted(MySimpleLinkedList l1, MySimpleLinkedList l2) {
 		MySimpleLinkedList resultList = new MySimpleLinkedList();
 		int size = l1.size();
-		Node tmp = l1.firstNode();	
-		l1.resetCursor();
+		l1.resetCursor();			
 		for (int i=0; i<size; i++) {
+			Integer tmp = l1.get();
 			l2.resetCursor();
-			if (l2.has(tmp.getInfo())) {
-				resultList.insertSorted(tmp.getInfo());
+			if (l2.has(tmp)) {
+				resultList.insertSorted(tmp);
 			}
-			tmp = tmp.getNext();
 		}
+		resultList.resetCursor();
+		return resultList;		
+	}
+	
+	private static MySimpleLinkedList mergeSortedLists(MySimpleLinkedList l1, MySimpleLinkedList l2) {
+		MySimpleLinkedList resultList = new MySimpleLinkedList();
+		int size = l1.size();
+		l1.resetCursor();	
+		l2.resetCursor();
+		for (int i=0; i<size; i++) {
+			Integer tmp = l1.get();
+			if (l2.hasForSorted(tmp)) {
+				resultList.insertFromSortedLists(tmp);
+			}
+		}
+		resultList.resetCursor();
 		return resultList;		
 	}
 
