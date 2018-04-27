0dbca90a6177edd247a4b7c77200d51326248489
MaggieMarchena <maga11_tandil@hotmail.com>
2018-04-27 11:04:20 -0300

    Comentarios agregados

--- Practico 2/Node.java
+++ Practico 2/Node.java
@@ -16,28 +16,31 @@ public class Node {
 
 	//SETTERS
 
+	//Sets a given Node as the childLeft
 	protected void setChildLeft(Node n) {
 		childLeft = n;
 	}
 	
+	//Sets a given Node as the childRight
 	protected void setChildRight(Node n) {
 		childRight = n;
 	}
 
 	//GETTERS
+	
+	//Returns the Integer value of the Node
 	protected Integer getInfo() {
 		return info;
 	}
 
+	//Returns the Node on childLeft
 	protected Node getChildLeft() {		
 		return childLeft;
 	}
 	
+	//Returns the Node on childRight
 	protected Node getChildRight() {		
 		return childRight;
 	}
-	
-	
-	//METHODS
 
 }
