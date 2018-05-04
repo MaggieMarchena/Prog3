473297ecc2c194e2b3d14ebaef1e1eecb9301a77
MaggieMarchena <maga11_tandil@hotmail.com>
2018-05-04 13:46:57 -0300

    Comentarios agregados

--- TPE/Primera entrega/src/library/Library.java
+++ TPE/Primera entrega/src/library/Library.java
@@ -13,21 +13,36 @@ public class Library {
 	private List<Book> books;
 	private ComparatorGenderName comparator;
 
+	//CONSTRUCTORS
 	public Library() {
 		this.index = new Index();
 		this.books = new ArrayList<Book>();
 		this.comparator = new ComparatorGenderName();
 	}
 	
+	//METHODS
+	
+	/**
+	 * Agrega el libro a la colección y al índice de la misma
+	 * @param b el libro que se va a agregar
+	 */
 	public void addBook(Book b){
 		this.index.add(b);
 		this.books.add(b);
 	}
 	
+	/**
+	 * Ordena la lista de géneros de forma alfabética
+	 */
 	public void sort(){
 		this.index.sort(this.comparator);
 	}
 	
+	/**
+	 * Inicia la búsqueda de los libros de un género en el índice
+	 * @param genderName nombre del género
+	 * @return List<Book>
+	 */
 	public List<Book> getBooksByGender(String genderName){
 		return this.index.getBooksByGender(genderName);
 	}
