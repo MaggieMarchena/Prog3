d9582227a23269c1f76a270b621e23c0f07476ba
MaggieMarchena <maga11_tandil@hotmail.com>
2018-05-04 13:45:50 -0300

    Comentarios agregados

--- TPE/Primera entrega/src/counter/IterationCounter.java
+++ TPE/Primera entrega/src/counter/IterationCounter.java
@@ -2,19 +2,33 @@ package counter;
 
 public class IterationCounter {
 	
+	//ATTRIBUTES
 	private int counter;
 
+	//CONSTRUCTORS
 	public IterationCounter() {
 	}
 	
+	//METHODS
+	
+	/**
+	 * Asigna valor 0 al counter
+	 */
 	public void start(){
 		this.counter = 0;
 	}
 	
+	/**
+	 * Agrega una unidad al counter
+	 */
 	public void add(){
 		this.counter++;
 	}
 	
+	/**
+	 * Devuelve el counter
+	 * @return int
+	 */
 	public int get(){
 		return this.counter;
 	}
