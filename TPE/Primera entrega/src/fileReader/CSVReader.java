d8440e3205d458c8354072fc77585ee62baccd2e
MaggieMarchena <maga11_tandil@hotmail.com>
2018-05-04 13:46:12 -0300

    Comentarios agregados

--- TPE/Primera entrega/src/fileReader/CSVReader.java
+++ TPE/Primera entrega/src/fileReader/CSVReader.java
@@ -7,22 +7,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 import components.Book;
+import counter.IterationCounter;
 import library.Library;
 
 public class CSVReader {
 	
+	//ATTRIBUTES
 	private List<String> fileNames;
 	private int counter;
 	private Library library;
-	boolean firstLine;
+	private boolean firstLine;
+	private IterationCounter itCounter;
 
+	//CONSTRUCTORS
     public CSVReader() {
 		this.fileNames = new ArrayList<String>();
 		this.counter = 0;
 		this.library = new Library();
 		this.firstLine = true;
+		this.itCounter = new IterationCounter();
 	}
 
+    //METHODS
+    
+    /**
+     * Agrega el archivo a la lista de archivos del reader, al leer el archivo se generan los libros 
+     * y se agregan conjuntamente a la colección y al índice de la misma
+     * @param fileName nombre del archivo que se agrega
+     */
 	public void addFile(String fileName) {
 		this.fileNames.add(fileName);
 		this.counter++;
@@ -45,8 +57,21 @@ public class CSVReader {
         this.library.sort();
     }
 	
+	/**
+	 * Inicia la búsqueda de los libros de un género desde la colección
+	 * @param genderName nombre del género 
+	 * @return List<Book>
+	 */
 	public List<Book> getBooksByGender(String genderName){
 		return this.library.getBooksByGender(genderName);
+	}
+	
+	/**
+	 * Devuelve el valor de iteraciones
+	 * @return int
+	 */
+	public IterationCounter getItCounter(){
+		return this.itCounter;
 	}
     
 }
