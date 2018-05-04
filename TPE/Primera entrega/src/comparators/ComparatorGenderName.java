db0a747920bf8866a20b7ce25a3437dfbd87c3d5
MaggieMarchena <maga11_tandil@hotmail.com>
2018-05-04 13:44:50 -0300

    Comentarios agregados

--- TPE/Primera entrega/src/comparators/ComparatorGenderName.java
+++ TPE/Primera entrega/src/comparators/ComparatorGenderName.java
@@ -6,10 +6,13 @@ import components.Gender;
 
 public class ComparatorGenderName implements Comparator<Gender> {
 
+	//CONSTRUCTORS
 	public ComparatorGenderName() {
-		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * Compara los nombres de los géneros para ordenarlos de forma alfabética
+	 */
 	@Override
 	public int compare(Gender g1, Gender g2) {
 		return (g1.getName().compareTo(g2.getName()));
